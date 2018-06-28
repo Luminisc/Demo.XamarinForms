a2a356b99845519b9fddd1985454d69e83e69341
Luminisc <luminsc@gmail.com>
2018-06-28 18:47:59 +0300

    init

---
+++ AndroidXAMLApp/AndroidXAMLApp.Android/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms;
+
+namespace AndroidXAMLApp.Droid
+{
+    [Activity(Label = "AndroidXAMLApp", 
+		Icon = "@mipmap/icon", 
+		Theme = "@style/MainTheme", 
+		MainLauncher = true, 
+		ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation
+		)]
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
